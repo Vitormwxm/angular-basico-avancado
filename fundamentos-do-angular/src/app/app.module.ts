1cd134a87a5e6a651973d7ea0a193baaba296aed
vitor <vitormwxm@gmail.com>
2025-10-26 20:50:29 -0300

    criando components de card

--- fundamentos-do-angular/src/app/app.module.ts
+++ fundamentos-do-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { CardRoxoComponent } from './components/card-roxo/card-roxo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent
   ],
   imports: [
     BrowserModule,
