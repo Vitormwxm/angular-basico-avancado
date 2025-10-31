17e97ed6774d3da978e39665298e74e023ef4c41
vitor <vitormwxm@gmail.com>
2025-10-30 23:13:22 -0300

    treinando style binding

--- fundamentos-do-angular/src/app/components/card-user/card-user.component.ts
+++ fundamentos-do-angular/src/app/components/card-user/card-user.component.ts
@@ -10,14 +10,28 @@ export class CardUserComponent {
   userName: string = "padrao";
   inputValue = "teste";
   attrBiding = "teste"
+  color = "none"
 
   alterUserName() {
     this.userName = "Novo"
     this.attrBiding = "Teste diferenciado"
+
+    const currentTarget = document.querySelector('#meu-input')?.innerHTML
+
+    if(currentTarget == '') {
+        this.color = "red"
+        console.log("alterou para " + this.color)
+    } 
+    
   }
 
   handleInputkeyUp(event: KeyboardEvent) {
     const currentTarget = (event.target as HTMLInputElement)
+
+    if(currentTarget.value != '') {
+      this.color = "white"
+      console.log("entrou" + this.color)
+    } 
     console.log(currentTarget.value)
   }
   
